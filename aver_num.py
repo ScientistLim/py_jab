b5e20cfde3fac985e103d0c91c4a1d77fc52d3e6
ScientistLim <48274148+ScientistLim@users.noreply.github.com>
2020-04-16 03:59:09 +0900

    Create aver_num.py

---
+++ aver_num.py
@@ -0,0 +1,15 @@
+import sys
+
+numList= []
+n = int(inpus("how many number":"))
+while n>0:
+        nn=int(input("input number:"))
+        numList.append(nn)
+        n = n-1
+total = 0;
+count = 0;
+for i in numList;
+        total +=i
+        count +=1
+print("average:")
+print(total/count)
